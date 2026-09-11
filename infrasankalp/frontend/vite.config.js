2ff9c40239f542d2f05905dd0b0d89097329d30f
Samarthpatil75 <samarthpatil8869@gmail.com>
2026-09-11 22:22:10 +0530

    Update vite.config.js

--- infrasankalp/frontend/vite.config.js
+++ infrasankalp/frontend/vite.config.js
@@ -6,9 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    base: '/SANKALP/',
     tailwindcss(),
   ],
+  base: '/SANKALP/',
   server: {
     port: 5173,
     proxy: {
